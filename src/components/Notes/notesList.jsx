7593f35830237df15308b1bc9604381f3d10aa49
Leidy Paola <paoalm1@hotmail.com>
2021-08-05 10:49:59 -0500

    funcion agregar notas desde formulario a firestore

--- src/components/Notes/notesList.jsx
+++ src/components/Notes/notesList.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFirestore } from 'reactfire';
 import '../style/Notes.css';
 import '../../pages/style/pages.css'
 import "firebase/firestore"
-import {  useFirestore } from 'reactfire';
+
 // import Notes from './notes';
-import { useState } from 'react';
+
 
 
 export default function NotesList() {
+
 	const db = useFirestore()
-	const [values, setValue]= useState({
-		title:"",
-		descripcion:""
-	})
-	const handleOnChange= (e)=>{
-		const { name, value } = e.target
-		setValue({...values, [name]: value})
-		
+	const [values, setValue] = useState(inicialValues)
+	const inicialValues = {
+		title: "",
+		descripcion: ""
 	}
+	
+	const handleOnChange = (e) => {
+		const { name, value } = e.target
+		setValue({ ...values, [name]: value })
+
+	}
+	//Añadir notas a firestore
+	const addNotes = async (notes) => {
+		try {
+			db.collection("Notes").add(notes)
+		}
+		catch (error) {
+			console.log(error)
+		}
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		db.collection("Notes").add({
-			name: "PRUEBA",
-			descripcion: "Prueba"
-		})
-		.then((docRef) => {
-			console.log("Document written with ID: ", docRef.id);
-		})
-		
+
+		addNotes(values);
+		setValue(inicialValues)
+
 	};
-	
+
 	return (
 		<div className="note-list">
-				{/* { notes.map((note)=>
+			{/* { notes.map((note)=>
 					<Notes
 					Title={note.Title}
 					Descripcion={note.Descripcion}
@@ -41,11 +51,11 @@ export default function NotesList() {
 				)} */}
 
 			<form className="form-notes note" onSubmit={handleSubmit}>
-				<input name="title" value={values.title} onChange={handleOnChange}className="titulo-nota" placeholder="Titulo..."/>
+				<input name="title" value={values.title} onChange={handleOnChange} className="titulo-nota" placeholder="Titulo..." />
 				<textarea name="descripcion" value={values.descripcion} onChange={handleOnChange} className="Description-note" ></textarea>
-				<input className="submit" value="ok" type="submit"/> 
+				<input className="submit" value="ok" type="submit" />
 			</form>
-		
+
 		</div>
 	)
 }
@@ -58,3 +68,10 @@ export default function NotesList() {
 	// .then((docRef) => {
 	// 	console.log("Document written with ID: ", docRef.id);
 	// })
+	// if (currentId=== ""){
+			// 	await db.collection("Notes").doc().set(notes);
+			// }
+			// else{
+			// 	await db.collection("Notes").doc(currentId).update(notes);
+			// setCurrentId("")
+			// }
